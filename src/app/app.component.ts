c49b70b77c8121d320594622b1ff447403a86de9
Matheus.Juliao <matheus.juliao@stoneridge.com>
2022-10-13 10:37:02 -0300

    Finish class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   	selector: 'app-root',
   	template: `
-  		<!-- <router-outlet></router-outlet> -->
+  		<router-outlet></router-outlet>
 		<!-- <app-food-add></app-food-add>
         <app-food-list></app-food-list> -->
-		<app-forms></app-forms>
+		<!-- <app-forms></app-forms> -->
 	`
 })
 export class AppComponent {}
